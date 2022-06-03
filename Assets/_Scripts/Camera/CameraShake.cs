fccaf5e20b850a41f3af5eda845b938d84221cf9
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-06-03 09:59:47 +0545

    Added Bomb Bird Blast Particle effect

--- Assets/_Scripts/Camera/CameraShake.cs
+++ Assets/_Scripts/Camera/CameraShake.cs
@@ -6,9 +6,10 @@ public class CameraShake : MonoBehaviour
     public IEnumerator Shake(float duration, float magnitude)
     {
         Vector3 originalPosition = transform.localPosition;
+
         float elapsedTime = 0;
 
-        while(elapsedTime < duration)
+        while(elapsedTime <= duration)
         {
             Vector2 randomValue = Random.insideUnitCircle * magnitude;
             transform.localPosition = new Vector3(randomValue.x, randomValue.y, originalPosition.z);
